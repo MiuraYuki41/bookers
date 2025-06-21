deb21045d49a51a17e7809622a6c0047e23e3ce5
MiuraYuki41 <205943986+MiuraYuki41@users.noreply.github.com>
2025-06-21 09:15:15 +0000

    commitnew

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   #get 'show' => 'books/show'
   #get 'edit' => 'books/edit'
   #get 'books' => 'books#index'
-  get 'top' => 'homes#top'
+  root to: 'homes#top'
+  #get 'top' => 'homes#top'
  # post 'books' => 'books#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books
